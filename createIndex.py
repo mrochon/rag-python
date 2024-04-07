c6713744e4e61456143af4bd43fcc2405c26ef7b
MariusR <mrochon@microsoft.com>
2024-04-07 15:14:26 -0700

    Moved createIndex

--- createIndex.py
+++ createIndex.py
@@ -4,14 +4,15 @@ import requests
 
 dotenv.load_dotenv()
 
+TENANT_ID = os.environ.get("TENANT_ID")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-TOKEN_URL = "https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token" 
+TOKEN_URL = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token" 
 SEARCH_SERVICE_NAME = os.environ.get("SEARCH_SERVICE_NAME")
 SEARCH_API_KEY=os.environ.get("SEARCH_API_KEY")
 rest_url = f"https://{SEARCH_SERVICE_NAME}.search.windows.net/indexes?api-version=2023-11-01"
 
-file_path = os.path.join(os.getcwd(), "REST/createIndex.json")
+file_path = os.path.join(os.getcwd(), "data/createIndex.json")
 with open(file_path, "r") as file:
     createIndex= file.read()
 
@@ -19,7 +20,7 @@ with open(file_path, "r") as file:
 # oauth_session = OAuth2Session(client_id=CLIENT_ID)
 # token = oauth_session.fetch_token(grant_type="client_credentials", client_secret=CLIENT_SECRET, token_url=TOKEN_URL, authorization_response=)
 
-# request = f"grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&scope=https://management.azure.com/.default"
+# request = f"grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&scope=https://cognitiveservices.azure.com/.default"
 # tokens = requests.post(url=TOKEN_URL, data=request, headers={"Content-Type": "application/x-www-form-urlencoded"})
 # token = tokens.json().get("access_token")
 # headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
