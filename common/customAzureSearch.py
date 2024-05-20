86e68c55e58dd6fe8733112c2f3e196e29179f92
MariusR <mrochon@microsoft.com>
2024-05-19 21:28:27 -0700

    Fixe3d display

--- common/customAzureSearch.py
+++ common/customAzureSearch.py
@@ -24,7 +24,7 @@ def get_search_results(query: str, indexes: list, token: str,
     for index in indexes:
         search_payload = {
             "search": query,
-            "select": "id, uri, chunk",
+            "select": "uri, chunk",
             "queryType": "semantic",
             "vectorQueries": [{"text": query, "fields": "chunkVector", "kind": "text", "k": k}],
             "semanticConfiguration": "manuals-semantic-configuration",
@@ -43,16 +43,18 @@ def get_search_results(query: str, indexes: list, token: str,
     content = dict()
     ordered_content = OrderedDict()
     
+    id = 0
     for index,search_results in agg_search_results.items():
         for result in search_results['value']:
             if result['@search.rerankerScore'] > reranker_threshold: # Show results that are at least N% of the max possible score=4
-                content[result['id']]={
-                                        "uri": result['uri'], 
-                                        "chunk": result['chunk'],
-                                        "caption": result['@search.captions'][0]['text'],
-                                        "score": result['@search.rerankerScore'],
-                                        "index": index
-                                    }
+                content[id]={
+                                "uri": result['uri'], 
+                                "chunk": result['chunk'],
+                                "caption": result['@search.captions'][0]['text'],
+                                "score": result['@search.rerankerScore'],
+                                "index": index
+                            }
+                id += 1
                 
 
     topk = k
@@ -79,12 +81,11 @@ class CustomAzureSearchRetriever(BaseRetriever):
     ) -> List[Document]:
         
         token_resp = self.credential.get_token("https://search.azure.com/.default")
-        #token_resp = {'token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkwxS2ZLRklfam5YYndXYzIyeFp4dzFzVUhIMCIsImtpZCI6IkwxS2ZLRklfam5YYndXYzIyeFp4dzFzVUhIMCJ9.eyJhdWQiOiJodHRwczovL3NlYXJjaC5henVyZS5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9jMzY0NWUwOS1kNjAyLTRlMjUtOTUwYy01ODUwZTM4M2Q2ZjIvIiwiaWF0IjoxNzE2MTc0NzExLCJuYmYiOjE3MTYxNzQ3MTEsImV4cCI6MTcxNjE3ODYxMSwiYWlvIjoiRTJOZ1lQRFMzbnV3NjhjWDhRK3liVkl2ODE4dUF3QT0iLCJhcHBpZCI6IjlkOGYzZWFiLWFkMDUtNDg0ZC05Y2VjLWFhZDUyYTkzOTJkNCIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2MzNjQ1ZTA5LWQ2MDItNGUyNS05NTBjLTU4NTBlMzgzZDZmMi8iLCJpZHR5cCI6ImFwcCIsIm9pZCI6ImZlNzdjYzM1LTg4YjQtNGQ3ZS05YjNiLTI0ODIzZDIwNGEwNyIsInJoIjoiMC5BWDBBQ1Y1a3d3TFdKVTZWREZoUTQ0UFc4b0NqRFloZW1KaEJnYm5nV3h6Rk1WaWNBQUEuIiwic3ViIjoiZmU3N2NjMzUtODhiNC00ZDdlLTliM2ItMjQ4MjNkMjA0YTA3IiwidGlkIjoiYzM2NDVlMDktZDYwMi00ZTI1LTk1MGMtNTg1MGUzODNkNmYyIiwidXRpIjoiYVVMNjRaRWpSME9WMzc5M2M3MHlBQSIsInZlciI6IjEuMCJ9.Om2nxL6DyZW3czY-Yuk-kUcqrBVPFkdAnZv5sJeoEqVSPnP_hOawxrc9FPUKY98UjjLFmq2xO9gEoAfMgcnx0TRW-GEyj2QEGBxjYH0rr8vzZHNN2no3I027VpGYjIwO2AchHxl8f8lCQJc4yt3zaT97ynicSP2GcIyqHLihluV4S_EcOaYfpcvgQhQVlDNuLHlA9EGAOJpwFu5AmKknbGMHoEh-cvKF3xipn5zm8FxPx_AZNcSSVXQOb-ndDIbadOYwtiFzW_atRCUbJ-3yjxfCd08F1pwqKlwDVx01NLCylb5G8Lqam2FGQyjyMrjBuFUR6if3SZnU26l8DVI-jw'}
         ordered_results = get_search_results(query, self.indexes, token_resp.token, k=self.topK, reranker_threshold=self.reranker_threshold, )
         
         top_docs = []
         for key,value in ordered_results.items():
-            location = value["location"] if value["location"] is not None else ""
+            location = value["uri"] if value["uri"] is not None else ""
             top_docs.append(Document(page_content=value["chunk"], metadata={"source": location, "score":value["score"]}))
 
         return top_docs
--- search.py
+++ search.py
@@ -5,11 +5,39 @@ import requests
 import json
 from langchain_core.output_parsers import StrOutputParser
 from operator import itemgetter
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
 
 dotenv.load_dotenv('.env', verbose=True, override=True)
 
 from common.customAzureSearch import CustomAzureSearchRetriever
 from azure.identity import DefaultAzureCredential
+
+DOCSEARCH_PROMPT_TEXT = """
+
+## On your ability to answer question based on fetched documents (sources):
+- Given extracted parts (CONTEXT) from one or multiple documents, and a question, Answer the question thoroughly with citations/references. 
+- If there are conflicting information or multiple definitions or explanations, detail them all in your answer.
+- In your answer, **You MUST use** all relevant extracted parts that are relevant to the question.
+- **YOU MUST** place inline citations directly after the sentence they support using this Markdown format: `[[number]](url)`.
+- The reference must be from the `source:` section of the extracted parts. You are not to make a reference from the content, only from the `source:` of the extract parts.
+- Reference document's URL can include query parameters. Include these references in the document URL using this Markdown format: [[number]](url?query_parameters)
+- **You MUST ONLY answer the question from information contained in the extracted parts (CONTEXT) below**, DO NOT use your prior knowledge.
+- Never provide an answer without references.
+- You will be seriously penalized with negative 10000 dollars if you don't provide citations/references in your final answer.
+- You will be rewarded 10000 dollars if you provide citations/references on paragraph and sentences.
+- **You must** respond in the same language as the question, regardless of the language of the CONTEXT
+
+
+- Remember to respond in the same language as the question
+"""
+
+DOCSEARCH_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", DOCSEARCH_PROMPT_TEXT + "\n\nCONTEXT:\n{context}\n\n"),
+        MessagesPlaceholder(variable_name="history", optional=True),
+        ("human", "{question}"),
+    ]
+)
 
 cred = DefaultAzureCredential()
 retriever = CustomAzureSearchRetriever(indexes=[os.environ['INDEX_NAME']], credential=cred, topK=5, reranker_threshold=1)
@@ -18,8 +46,9 @@ chain = (
         "context": itemgetter("question") | retriever, # Passes the question to the retriever and the results are assign to context
         "question": itemgetter("question")
     }
-    | StrOutputParser() 
+    | DOCSEARCH_PROMPT
 )
-chain.invoke({"question": "Which fillaments does Longer recommend?"})
+resp = chain.invoke({"question": "Which fillaments does Longer recommend?"})
+print(resp)
 # for chunk in chain.stream({"question": "What is the population of Denmark?"}):
 #     print(chunk, end="", flush=True)
